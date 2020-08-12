038cb0354c38002e4f520d5723efed737711e3ba
John Hunt <john.hunt10@gmail.com>
2020-08-12 14:50:32 +0100

    Simple hello world example applications

---
+++ 02-hello-world/src/hello2.kt
@@ -0,0 +1,10 @@
+/**
+ * Interactive version of the simple
+ * hello world program.
+ */
+fun main() {
+    println("Hello World!")
+    print("Please Enter your Name: ")
+    val name = readLine()
+    println("Hello $name")
+}
