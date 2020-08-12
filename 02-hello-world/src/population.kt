fc0e8745cd6c3415c50c2065d24eac527fb24f8b
John Hunt <john.hunt10@gmail.com>
2020-08-12 15:09:35 +0100

    Code layout example

---
+++ 02-hello-world/src/population.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val city = "London"
+    val numberOfPeopleInCity = 6500000
+    val year = 1900
+
+    println("The total population of " +
+            city +
+            " was " +
+            numberOfPeopleInCity +
+            " in " + year)
+}
