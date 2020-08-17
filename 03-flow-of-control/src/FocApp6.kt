fb5a027deb5ae8942de4314ded2d43f85b39f790
John Hunt <john.hunt10@gmail.com>
2020-08-17 14:00:11 +0100

    Added nested if example

---
+++ 03-flow-of-control/src/FocApp6.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val snowing = true
+    val temp = -1
+
+    if (temp < 0) {
+        println("It is freezing")
+        if (snowing) {
+            println("Put on boots")
+        }
+        println("Time for Hot Chocolate")
+    }
+
+    println("Bye")
+}
