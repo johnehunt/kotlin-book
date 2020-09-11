011a19c14c143345c4b6106b046d36c1c920ee3d
John Hunt <john.hunt10@gmail.com>
2020-09-11 11:50:37 +0100

    Numeric operatiosn examples

---
+++ 03-flow-of-control/src/numeric-operators.kt
@@ -0,0 +1,33 @@
+fun main() {
+
+    // Addition Operator
+    println(1 + 2)
+    println(2.2 + 3.4)
+    println("Hello " + "World")
+    println("Hello" + 2)
+    // compile time error as tries to do numeric addition with a string
+    // println(2 + "hello")
+
+    // Subtraction operator
+    println(2 - 1)
+    println(2.1 - 1)
+    println(3 - 1.5)
+
+    // Multiplication Operator
+    println(2 * 3)
+    println(2.5 * 3.4)
+    println(3 * 2.5)
+
+    // Division Operator
+    println(6 / 3)
+    println(5 / 2)
+    println(5 / 2.3)
+    println(3.4 / 1.2)
+
+    // Modulus Operator
+    println(6 % 3)
+    println(5 % 2)
+    println(5 % 2.3)
+    println(3.4 % 1.2)
+
+}
