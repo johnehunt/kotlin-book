abf4db529818581e02a0b7dde5b0148c9eaef146
John Hunt <john.hunt10@gmail.com>
2021-01-16 12:19:24 +0000

    Showing closing a property

--- 11-inheritance/src/Food.kt
+++ 11-inheritance/src/Food.kt
@@ -1,11 +1,17 @@
-open class Food {
+open class Food(open val type: String = "generic") {
     open fun eatMe() = println("Food - eatMe()")
 }
 
-open class Cake: Food() {
+open class Cake(final override val type: String = "Sponge"): Food() {
     final override fun eatMe() = println("Cake - eatMe()")
 }
 
-open class Biscuit: Food() {
+open class Biscuit(override val type: String = "Sweet"): Food() {
     override fun eatMe() = println("Biscuit - eatMe()")
+}
+
+fun main() {
+    val c = Cake()
+    println(c.type)
+    c.eatMe()
 }
