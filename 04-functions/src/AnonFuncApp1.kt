dece2c8cbbcfc3bd49dfd7a2bd4ae19fcc1510f5
John Hunt <john.hunt10@gmail.com>
2021-01-26 14:10:21 +0000

    Added further anonymous function examples

--- 04-functions/src/AnonFuncApp1.kt
+++ 04-functions/src/AnonFuncApp1.kt
@@ -2,4 +2,15 @@ val func = fun(i: Int): Int { return i + 1 }
 
 fun main() {
     println(func(5))
+
+    val increment = fun(i: Int): Int {
+        return i + 1
+    }
+    val decrement = fun(i: Int): Int = i - 1
+    val double = fun(i: Int) = i * 2
+
+    println(increment(5))
+    println(decrement(5))
+    println(double(5))
+
 }
