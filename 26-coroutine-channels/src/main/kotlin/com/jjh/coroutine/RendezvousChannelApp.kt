f3d16f69846d929d1b5f994316bdbcb9a8d6d6a2
John Hunt <john.hunt10@gmail.com>
2021-02-03 11:59:13 +0000

    Refactoed example to encapsulate functions

--- 26-coroutine-channels/src/main/kotlin/com/jjh/coroutine/RendezvousChannelApp.kt
+++ 26-coroutine-channels/src/main/kotlin/com/jjh/coroutine/RendezvousChannelApp.kt
@@ -5,23 +5,23 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-val channel = Channel<Int>() // Shared rendezvous Channel
-
-suspend fun sendDataToChannelTask() {
-    repeat(3) {
-        delay(100)
-        println("Sending ---> $it")
-        channel.send(it)
-    }
-}
-
-suspend fun receiveDataFromChannelTask() {
-    repeat(3) {
-        println("Receiving ---> ${channel.receive()}")
-    }
-}
-
 fun main() {
+    val channel = Channel<Int>() // Shared rendezvous Channel
+
+    suspend fun sendDataToChannelTask() {
+        repeat(3) {
+            delay(100)
+            println("Sending ---> $it")
+            channel.send(it)
+        }
+    }
+
+    suspend fun receiveDataFromChannelTask() {
+        repeat(3) {
+            println("Receiving ---> ${channel.receive()}")
+        }
+    }
+
     println("Main -> Launching rendezvous channel task")
     GlobalScope.launch { sendDataToChannelTask() }
     GlobalScope.launch { receiveDataFromChannelTask() }
