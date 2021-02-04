fe73fbee6da6624a1898f1c04da7454239db14fd
John Hunt <john.hunt10@gmail.com>
2021-02-04 10:58:27 +0000

    Removed reference to args

--- 29-rxkotlin-flowables-and-concurrency/src/main/kotlin/com/jjh/react/SimpleConcurrentObserversApp.kt
+++ 29-rxkotlin-flowables-and-concurrency/src/main/kotlin/com/jjh/react/SimpleConcurrentObserversApp.kt
@@ -3,7 +3,7 @@ package com.jjh.react
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-fun main(args: Array<String>) {
+fun main() {
 
     // An alternative approach
     Flowable.range(1, 10)
