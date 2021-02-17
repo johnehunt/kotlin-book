6895fdc84ce952a8a583c2f3dba82d3b9dd962ad
John Hunt <john.hunt10@gmail.com>
2021-02-17 11:12:48 +0000

    Added examples of concurrent and non concurrent observables

--- 29-rxkotlin-flowables-and-concurrency/src/main/kotlin/com/jjh/react/CurrentObservableApp.kt
+++ 29-rxkotlin-flowables-and-concurrency/src/main/kotlin/com/jjh/react/CurrentObservableApp.kt
@@ -1,6 +1,7 @@
 package com.jjh.react
 
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 fun main() {
     Observable.create<Int> { emitter ->
@@ -11,9 +12,12 @@ fun main() {
             ++currentNumber
         }
         emitter.onComplete()
-    }.subscribe{println("value received $it")}  // on next value emitted handler
+    }.subscribeOn(Schedulers.newThread())  // run subscription in new thread
+        .observeOn(Schedulers.computation()) // run observers in computation thread
+        .subscribe{println("value received $it")}
 
     // Wait for the observable to finish
     println("Press enter to terminate")
     readLine()
+
 }
