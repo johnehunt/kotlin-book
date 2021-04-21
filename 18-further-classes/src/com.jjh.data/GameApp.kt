b64dc06f9f3cb1a175672cba537e4e89047d24ec
John Hunt <john.hunt10@gmail.com>
2021-04-21 11:22:37 +0100

    Updated ship data class example

--- 18-further-classes/src/com.jjh.data/GameApp.kt
+++ 18-further-classes/src/com.jjh.data/GameApp.kt
@@ -4,35 +4,36 @@ fun main() {
     val inst1 = GameContent(10.0, 10.0)
     println(inst1)
     val inst2 = GameContent(10.0, 10.0)
-    println(inst2) // uses default implementation of toString()
+    // uses default implementation of toString()
+    println(inst2)
     println("x: ${inst2.x}, obj2.y: ${inst2.y}")
     println(inst1 == inst2) // uses default implementation of equals()
     println(inst1.hashCode())
 
     println("-------")
 
-    val ship = Ship(10.0, 10.0)
-    println(ship)
-    println(ship.image)
-    ship.image = "default.png"
-    println(ship)
+    val ship1 = Ship(10.0, 10.0)
+    println(ship1)
+    println(ship1.image)
+    ship1.image = "default.png"
+    println(ship1)
     val ship2 = Ship(10.0, 10.0)
-    println(ship == ship2)
+    println(ship1 == ship2)
 
     println("------")
     // copying
 
-    ship.image = "default.png"
-    println("ship: $ship")
-    println("ship.image: ${ship.image}")
-    val otherShip = ship.copy(x = 20.0)
+    ship1.image = "default.png"
+    println("ship: $ship1")
+    println("ship.image: ${ship1.image}")
+    val otherShip = ship1.copy(x = 20.0)
     println("othership: $otherShip")
     println("otherShip.image: ${otherShip.image}")
 
     println("------")
 
     // Data classes can be destructured
-    val (x, y) = ship
+    val (x, y) = ship1
     println("x $x, y $y")
 
     var (a, b) = otherShip
@@ -40,7 +41,7 @@ fun main() {
 
     // can ignore a parameter if desired
 
-    val (_, y1) = ship
+    val (_, y1) = ship1
     println("y1: $y1")
 
 }
