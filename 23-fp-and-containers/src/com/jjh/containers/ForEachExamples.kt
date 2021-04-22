136cb3fe23fc0753b3f99ff5a2ee3ce75c0120e0
John Hunt <john.hunt10@gmail.com>
2021-04-22 09:04:00 +0100

    CHanges to layout and print statements

--- 23-fp-and-containers/src/com/jjh/containers/ForEachExamples.kt
+++ 23-fp-and-containers/src/com/jjh/containers/ForEachExamples.kt
@@ -1,6 +1,8 @@
 package com.jjh.containers
 
 fun main() {
+    println("forEach with Lists")
+
     val myList = listOf("Zero", "One", "Two")
 
     // Using a named function via a callable reference
@@ -13,9 +15,15 @@ fun main() {
     myList.forEach { s -> println(s) }
     myList.forEach { println(it) }
 
+    println("-------------")
+    println("forEach with arrays")
+
     // Using forEach with an array
     val myArray = arrayOf("Adam", "Jasmine", "Phoebe", "Gryff")
     myArray.forEach { println(it) }
+
+    println("-------------")
+    println("forEach with a map")
 
     // Using forEach with a map
     val map = mutableMapOf(
@@ -28,8 +36,11 @@ fun main() {
     print("\nValues: ")
     map.values.forEach { print("$it, ") }
     print("\nKey-Value pairs: ")
-    map.forEach{ k, v -> print("$k -> $v, ") }
+    map.forEach{ (k, v) -> print("$k -> $v, ") }
     println()
+
+    println("-------------")
+    println("forEachIndexed with a list")
 
     // Accessing the index as well as the element
     myList.forEachIndexed { i, s -> println("index: $i value: $s") }
