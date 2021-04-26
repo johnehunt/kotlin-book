bd336f48faa5f16fd83869bd49ed98ffe3c1385d
John Hunt <john.hunt10@gmail.com>
2021-04-26 11:48:07 +0100

    Solutions to chapter 2 exercises

---
+++ 02a-exercises/src/HelloWorld.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("Hello Kotlin World!")
+    print("Please Enter your Name: ")
+    val name = readLine()
+    println("Hello $name")
+    val total = 4 + 5
+    println(total)
+}
