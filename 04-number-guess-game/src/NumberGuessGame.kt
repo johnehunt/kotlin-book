456560d74ed7b5d35484df984108d9e4619afb56
John Hunt <john.hunt10@gmail.com>
2021-04-26 12:48:32 +0100

    Number guess game

---
+++ 04-number-guess-game/src/NumberGuessGame.kt
@@ -0,0 +1,42 @@
+import kotlin.random.Random
+
+val numberToGuess = Random.nextInt(0, 11)
+
+fun main() {
+
+    println("Welcome to the number guess game")
+
+    print("Please guess a number between 1 and 10: ")
+    val guess:String = readLine() ?: ""
+    var intGuess = guess.toInt()
+
+    var countNumberOfTries = 1
+    while (numberToGuess != intGuess) {
+        println("Sorry wrong number")
+
+        if (countNumberOfTries == 4) {
+            break
+        } else if (intGuess < numberToGuess) {
+            println("You guess was lower than the number")
+        } else {
+            println("Your guess was higher than the number")
+        }
+
+        print("Please guess again between 1 and 10: ")
+        val guess = readLine() ?: ""
+        intGuess = guess.toInt()
+        countNumberOfTries++
+    }
+
+    if (numberToGuess == intGuess) {
+        println("Well Done You Won!")
+        println("You took $countNumberOfTries goes to complete the game")
+    } else {
+        println("Sorry - You Loose")
+        println("The number you needed to guess was $numberToGuess")
+    }
+
+    println("Game Over")
+
+}
+
