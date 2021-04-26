49bb4985bdf35c6322658e67c23b0d1bda3eeeb6
John Hunt <john.hunt10@gmail.com>
2021-04-26 14:45:58 +0100

    Account class sample solution

---
+++ 08a-exercises/src/Account.kt
@@ -0,0 +1,33 @@
+
+class Account(
+    val accountNumber: String,
+    val holder: String,
+    var balance: Double,
+    val type: String) {
+
+    fun deposit(amount: Double) {
+        balance += amount
+    }
+    fun withdraw(amount: Double) {
+        balance -= amount
+    }
+
+    override fun toString(): String {
+        return "Account('$accountNumber', '$holder', $balance, type='$type')"
+    }
+
+}
+
+fun main() {
+    val acc1 = Account("123", "John", 10.05, "current")
+    val acc2 = Account("345", "John", 23.55, "savings")
+    val acc3 = Account("567", "Phoebe", 12.45, "investment")
+
+    println(acc1)
+    println(acc2)
+    println(acc3)
+
+    acc1.deposit(23.45)
+    acc1.withdraw(12.33)
+    println("balance: ${acc1.balance}")
+}
