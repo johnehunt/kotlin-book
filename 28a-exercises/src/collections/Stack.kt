9dfc541c93b13d7e7aee289408103538faa23134
John Hunt <john.hunt10@gmail.com>
2021-06-03 19:26:44 +0100

    Renaming

--- 28a-exercises/src/collections/Stack.kt
+++ 28a-exercises/src/collections/Stack.kt
@@ -18,29 +18,18 @@ class Stack<T>(val contents: MutableList<T> = mutableListOf<T>()) {
 
 }
 
-fun prettyPrintItem(element: String) {
-    println("\telement: '$element'")
-}
-
 fun main() {
     val stack = Stack<String>()
+    println("stack.isEmpty: ${stack.isEmpty}")
     stack.push("Task1")
     stack.push("Task2")
     stack.push("Job1")
     stack.push("Task3")
     stack.push("Job2")
     println(stack)
-
-    // Apply functions to stack contents using forEach, map and filter
-    println("Printing the stack contents using forEach")
-    stack.contents.forEach(::prettyPrintItem)
-
-    println("Stack contents as uppercase strings")
-    val capsList = stack.contents.map{ it.toUpperCase() }
-    println(capsList)
-
-    println("Found all elements representing Jobs")
-    val filteredList = stack.contents.filter{ it.startsWith("Job") }
-    println(filteredList)
-
+    println("stack.size: ${stack.size}")
+    println("stack.isEmpty: ${stack.isEmpty}")
+    println("Stack.top(): ${stack.top()}")
+    println("Stack.top(): ${stack.pop()}")
+    println(stack)
 }
